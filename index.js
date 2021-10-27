aafba5f593f73395e5dda74d6806083ec0ee83ce
Lontchi Frank <fodjof6@gmail.com>
2021-10-27 13:32:50 +0100

    my server done

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 // compatible API routes.
 
 const express = require('express');
+// const ParseDashboard = require('parse-dashboard');
 const ParseServer = require('parse-server').ParseServer;
 const path = require('path');
 const args = process.argv || [];
@@ -16,7 +17,7 @@ const config = {
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
+  masterKey: process.env.MASTER_KEY || 'dev', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse', // Don't forget to change to https if needed
   liveQuery: {
     classNames: ['Posts', 'Comments'], // List of classes to support for query subscriptions
@@ -25,8 +26,19 @@ const config = {
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
 // javascriptKey, restAPIKey, dotNetKey, clientKey
-
+// const dashboard = new ParseDashboard({
+//   apps: [
+//     {
+//       serverURL: 'http://localhost:1337/parse',
+//       appId: 'myAppId',
+//       masterKey: 'myMasterKey',
+//       appName: 'MyApp',
+//     },
+//   ],
+// });
 const app = express();
+
+// app.use('/dashboard', dashboard);
 
 // Serve static assets from the /public folder
 app.use('/public', express.static(path.join(__dirname, '/public')));
